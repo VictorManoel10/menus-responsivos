60f444aa3842a1347b8d1475c67f6bee6d5b0d6e
Victor Manoel <victolopes2002@gmail.com>
2023-06-12 09:49:02 -0300

    att

---
+++ menu-3/script.js
@@ -0,0 +1,8 @@
+function openMenu() {
+    let menu = document.querySelector('.mobile-menu')
+    if(menu.classList.contains('open')) {
+        menu.classList.remove('open')
+    }else {
+        menu.classList.add('open')
+    }
+}
